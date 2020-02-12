e6cbdd5871f80f198706f9761b5e45db41ec8f79
jadekang <jkang.se@gmail.com>
2020-02-11 19:00:24 -0800

    main intro section styling is applied

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -17,8 +17,8 @@ function LandingPage() {
       <div id="ar-about">
         <div class="about-intro">
           <h4>About Me</h4>
-          <h1>I'm Patrick Harrison</h1>
-          <h3>Photographer/ Digital Artist</h3>
+          <h1>I'm Jade Kang</h1>
+          <h3>Software Engineer / Accountant</h3>
           <p>
             Eu euismod placerat mandamus sea, autem timeam hendrerit eu vel, sea
             debet conclusionemque te. Veniam persius suavitate sit cu. Brute
@@ -58,24 +58,44 @@ function LandingPage() {
       </div>
       <div className="main">
         <section className="split-home">
-          <section clssName="left-section personal wow fadeIn"></section>
+          <section
+            clssName="left-section personal wow fadeIn"
+            data-wow-delay="0.2s"
+          ></section>
           <section className="right-section">
             <div className="main-logo">
-              <img src={logo} width="56" height="56" alt="logo" />
+              <a className="wow fadeIn" data-wow-delay="0.3s" href="#">
+                <img src={logo} width="56" height="56" alt="logo" />
+              </a>
             </div>
             <div className="intro">
               <div className="intro-text">
-                <h4>Software Engineer based in California</h4>
-                <h1>Jade Kang</h1>
-                <p>
-                  Everything does something good, only a few make remarkable
-                  things. So, I built this portfolio to showcase my talent in
+                <h4 className="wow fadeInDown" data-wow-delay="0.4s">
+                  Software Engineer based in California
+                </h4>
+                <h1 className="wow fadeInDown" data-wow-delay="0.5s">
+                  Jade Kang
+                </h1>
+                <p className="wow fadeInDown" data-wow-delay="0.6s">
+                  Everything does something good, only a few make remarkable{' '}
+                  <br className="visible-lg" />
+                  things. So, I built this portfolio to showcase my talent in{' '}
+                  <br className="visible-lg" />
                   the as honest medium as possible for you.
                 </p>
-                <a href="mailto:contact@domain.com">Contact Me</a>
+                <a
+                  className="btn-contact wow fadeInDown"
+                  data-wow-delay="0.6s"
+                  href="mailto:contact@domain.com"
+                >
+                  Contact Me
+                </a>
               </div>
-              <div>
-                <span>
+              <div
+                className="footer-personal text-center wow fadeInDown"
+                data-wow-delay="0.7s"
+              >
+                <span className="footer-credits">
                   &copy; Copyright 2020 Jade Kang. All Rights Reserved.
                 </span>
               </div>
