bd56aec02356e6b5061b6990ae8d1649f03f6d58
jadekang <jkang.se@gmail.com>
2020-03-19 23:12:43 -0700

    finished the left component layout, UI, background color combinations, and rebuilt to deploy

--- src/Components/MainIntroLeft.js
+++ src/Components/MainIntroLeft.js
@@ -1,57 +1,50 @@
 import React from 'react';
 import '../Styles/Intros.css';
 import '../Styles/MainIntroLeft.css';
+import Badge from 'react-bootstrap/Badge';
+import BingleImage from '../images/BINGLE.png';
 
 function MainIntroLeft() {
   return (
     <section className="left-section featured-container">
-      <div className="featured-card">
-        1
-        <img src="http://www.pixeden.com/media/k2/galleries/468/001-business-card-clip-brand-mock-up-vol-20-psd.jpg" />
+      <div className="featured-card one"></div>
+      <div className="featured-card startup-box">
+        <span>category</span>
+        <h2 className="title-section">startups</h2>
       </div>
-      <div className="featured-card">
-        2
+      <div className="featured-card two">
+        <img
+          src="http://www.graphicsfuel.com/wp-content/uploads/2015/11/branding-mockup-psd.jpg"
+          alt="robin"
+        />
+      </div>
+      <div className="featured-card three">
+        <img src={BingleImage} alt="bingle-logo" />
+      </div>
+      <div className="featured-card article-box">
+        <span>category</span>
+        <h2 className="title-section">articles</h2>
+      </div>
+
+      <div className="featured-card five">
         <img src="http://www.graphicsfuel.com/wp-content/uploads/2015/11/branding-mockup-psd.jpg" />
       </div>
-      <div className="featured-card">
-        3
+      <div className="featured-card six">
         <img src="http://www.pixeden.com/media/k2/galleries/511/001-business-card-mockup-vol-22-box-brand-psd.jpg" />
       </div>
-      <div className="featured-card">
-        4
+      <div className="featured-card seven">
         <img src="http://www.pixeden.com/media/k2/galleries/468/001-business-card-clip-brand-mock-up-vol-20-psd.jpg" />
       </div>
-      <div className="featured-card">
-        5
+
+      <div className="featured-card project-box ">
+        <span>category</span>
+        <h2 className="title-section">projects</h2>
+      </div>
+      <div className="featured-card nine">
         <img src="http://www.graphicsfuel.com/wp-content/uploads/2015/11/branding-mockup-psd.jpg" />
       </div>
-      <div className="featured-card">
-        6
-        <img src="http://www.pixeden.com/media/k2/galleries/511/001-business-card-mockup-vol-22-box-brand-psd.jpg" />
-      </div>
-      <div className="featured-card">
-        7
-        <img src="http://www.pixeden.com/media/k2/galleries/468/001-business-card-clip-brand-mock-up-vol-20-psd.jpg" />
-      </div>
-      <div className="featured-card">
-        8
+      <div className="featured-card ten">
         <img src="http://www.graphicsfuel.com/wp-content/uploads/2015/11/branding-mockup-psd.jpg" />
-      </div>
-      <div className="featured-card">
-        9
-        <img src="http://www.pixeden.com/media/k2/galleries/511/001-business-card-mockup-vol-22-box-brand-psd.jpg" />
-      </div>
-      <div className="featured-card">
-        10
-        <img src="http://www.pixeden.com/media/k2/galleries/468/001-business-card-clip-brand-mock-up-vol-20-psd.jpg" />
-      </div>
-      <div className="featured-card">
-        11
-        <img src="http://www.graphicsfuel.com/wp-content/uploads/2015/11/branding-mockup-psd.jpg" />
-      </div>
-      <div className="featured-card">
-        12
-        <img src="http://www.pixeden.com/media/k2/galleries/511/001-business-card-mockup-vol-22-box-brand-psd.jpg" />
       </div>
     </section>
   );
